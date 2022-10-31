9ce58ca4b9eb46fe90c3ed75520846252844c65a
David <david@futurmap.com>
2022-10-31 14:07:34 +0300

    Section 7

--- dicee-flutter-master/lib/main.dart
+++ dicee-flutter-master/lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,9 +16,59 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  const DicePage({Key? key}) : super(key: key);
+
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            //flex: 2,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextButton(
+                onPressed: () {
+                  setState(() {
+                    rollDices();
+                  });
+                },
+                child: Image.asset('images/dice$leftDiceNumber.png'),
+              ),
+            ),
+          ),
+          Expanded(
+            //flex: 1,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextButton(
+                onPressed: () {
+                  setState(() {
+                    rollDices();
+                  });
+                },
+                child: Image(
+                  image: AssetImage('images/dice$rightDiceNumber.png'),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void rollDices() {
+    leftDiceNumber = Random().nextInt(6) + 1;
+    rightDiceNumber = Random().nextInt(6) + 1;
   }
 }
